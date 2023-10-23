3950fb9943472bdc299b2c7e779ebbad88262ba6
gitTanzj <kalle.riit@gmail.com>
2023-10-23 10:11:58 +0300

    follow up

--- backend/handleMongo.ts
+++ backend/handleMongo.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 import { DB_CONNECTION } from './config'
 
 
@@ -9,5 +9,17 @@ export const getCollection = async (name: string) => {
     const collection = database.collection(name)
     const collectionFetched = await collection.find().toArray()
     return collectionFetched
+}
+
+export const addEntry = async (name:string, entry: {noteBody: string, associatedStory: string}) => {
+    const database = client.db('Wisdiphy')
+    const notesCollection = database.collection(name);
+    await notesCollection.insertOne(entry)
+}
+
+export const deleteEntry = async (name:string, id:string) => {
+    const database = client.db('Wisdiphy')
+    const notesCollection = database.collection(name);
+    await notesCollection.deleteOne({"_id" : new ObjectId(id)})
 }
 
--- backend/server.ts
+++ backend/server.ts
@@ -1,16 +1,17 @@
 import express, { Request, Response, NextFunction } from 'express';
-import { getCollection } from './handleMongo'
+import { getCollection, addEntry, deleteEntry } from './handleMongo'
 const app = express()
 const PORT = 8000
 
-
+// USE REQUESTS
 app.use(express.json())
 app.use((req:Request, res:Response, next:NextFunction) => {
     console.log(req.method, req.url)
     next()
 })
 
-app.get('/', async (req:Request, res:Response) => {
+// GET REQUEST FOR THE STORIES
+app.get('/stories', async (req:Request, res:Response) => {
     const stories = await getCollection('Stories')
     if (stories.length != 0) {
         res.send(stories).status(200)
@@ -19,6 +20,33 @@ app.get('/', async (req:Request, res:Response) => {
     }
 })
 
+// GET REQUEST FOR THE NOTES
+app.get('/notes', async (req:Request, res:Response) => {
+    const notes = await getCollection('Notes')
+    if (notes.length != 0) {
+        res.send(notes).status(200)
+    } else{
+        res.send([]).status(500)
+    }
+})
+
+// POST REQUEST FOR THE NOTES
+app.post('/notes', async (req:Request, res:Response) => {
+    console.log(req.body)
+    if (!req.body.noteBody || !req.body.associatedStory) {
+        res.status(400).json({ error: 'noteBody and associatedStory is required.' });
+        return;
+    }
+
+    const entry = {noteBody: req.body.noteBody, associatedStory: req.body.associatedStory}
+    addEntry('Notes', entry)
+})
+
+// DELETE REQUEST FOR THE NOTES
+app.delete('/api/delete_data', (req, res) => {
+    console.log(`DELETE REQUEST SENT FOR ${req.body.id}!`)
+    deleteEntry('Notes', req.body.id)
+})
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
